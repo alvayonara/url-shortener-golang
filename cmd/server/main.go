28caa2271c5b48dd488af3ece1d72a49e1040551
Alva Yonara Puramandya <alva.puramandya@gojek.com>
2026-01-19 11:39:59 +0700

    feat(rest): implement health check

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
+	fmt.Println("Server running properly.")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
